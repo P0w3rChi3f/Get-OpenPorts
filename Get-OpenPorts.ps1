1407050f5b4c541d2d088f3234658e9de3e0f488
Jay <honeycutt.james@gmail.com>
2022-01-25 06:41:34 -0500

    completed a functioning port scanner.

--- Get-OpenPorts.ps1
+++ Get-OpenPorts.ps1
@@ -5,10 +5,13 @@ This is an alternative way to the Test-Connection cmdlet.  I have a static list 
 
 # $IPList = read-host "Please enter the file path of the host list"
 
-$IPList = 1..255 | ForEach-Object {"10.10.10.$_"}
+$IPList = 1..255 | ForEach-Object {"10.4.1.$_"}
 
 $ReplyResults = @()
+$i = 0
 foreach ($node in ($IPList)){
+    $i += 1
+    Write-Progress “Scanning Network” -PercentComplete (($i/$IPList.Count)*100)
     $icmpresults = ping $node -n 1 
     try {
         $ReplyResults += ((($icmpresults | Select-String "reply" | Where-Object {$_ -notlike "*unreachable*"}).ToString()).Split(" ")[2]).TrimEnd(":")
@@ -16,13 +19,11 @@ foreach ($node in ($IPList)){
     catch {
         write-host "$node is not accessable"
     }
-
-    
 } 
-$ReplyResults #| out-file .\OnlineIPs.txt
+$ReplyResults | out-file .\OnlineIPs.txt
 
 #############################################
-# Adding a port scanner on here now
+# Adding a TCP port scanner on here now
 ##############################################
 
 <#
@@ -34,19 +35,27 @@ https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcpclient?view=ne
 #>
 
 # Create the TCP Socket variable using .Net ( Assuming I can do this with UDP as well)
-$IPS = "10.4.1.180", "127.0.0.1"
+# $IPS = "10.4.1.180", "127.0.0.1"
+
 $TCPports = 443,1434,139,22,8080
+$UDPports = 53,88,137,138,3702,4500,5050,5353,5355,22
 
 $OpenPortList  = @()
-
-foreach ($IP in $IPS){
+$it1 = 0
+$it2 = 0
+foreach ($IP in $ReplyResults){
+    $it1 += 1
+    #Write-Progress “Scaning Targets” -PercentComplete (($it1/$ReplyResults.Count)*100)
     foreach ($port in $TCPports){
+        $it2 += 1
+        #Write-Progress “Scanning Ports” -PercentComplete (($it2/$TCPports.Count)*100)
         $tcp = New-Object System.Net.Sockets.TcpClient
         $tcpConnection = $tcp.ConnectAsync($IP,$Port)
         $wait = $tcpConnection.AsyncWaitHandle.WaitOne(1000, $false)
         if (!$wait){
-            Write-host "$port is closed on $IP Machine"
+            Write-host "TCP $port is closed on $IP Machine"
             $OpenPort = [PSCustomObject]@{
+                Proto = "TCP"
                 Port = $port
                 Host = $IP
                 Status = "Closed"
@@ -54,50 +63,76 @@ foreach ($IP in $IPS){
             $OpenPortList += $OpenPort
         } # Close If Statemen
         else {
-            Write-host "$port is Open on $IP Machine"
+            Write-host "TCP $port is Open on $IP Machine"
             $OpenPort = [PSCustomObject]@{
+                Proto = "TCP"
                 Port = $port
                 Host = $IP
                 Status = "Open"
             }
-            
+            $OpenPortList += $OpenPort
         }# Close Else Statement
     $tcp.Dispose()
 
     } #close Port foreach loop
+
+############################################################
+# Start my UDP Loop Here
+############################################################
+
+    foreach ($port in $UDPports){
+        $udpObject = New-Object System.Net.Sockets.UdpClient
+        $udpObject.Client.ReceiveTimeout = 1000
+        $udpObject.Connect($IP, $port)
+        $ASCIItext = New-Object System.Text.ASCIIEncoding
+        $byte = $ASCIItext.GetBytes("My Payload")
+        [void]$udpObject.Send($byte, $byte.Length)
+
+        $remoteendpoint = New-Object system.net.ipendpoint([system.net.ipaddress]::Any, 0)
+
+        try {
+            $receivebytes = $udpobject.Receive([ref]$remoteendpoint)
+            [string]$returndata = $ASCIItext.GetString($receivebytes)
+            
+            if ($returndata) {
+                Write-host "UDP $port is Open on $IP Machine"
+                $OpenPort = [PSCustomObject]@{
+                    Proto = "UDP"
+                    Port = $port
+                    Host = $IP
+                    Status = "Open"
+                }
+                $OpenPortList += $OpenPort
+                $udpObject.Close()
+            }
+        } # Close try
+        Catch {
+            If ($Error[0].ToString() -match "failed to respond") {
+                if ((Get-CimInstance Win32_PingStatus -Filter "(address='$IP' and timeout=10000)").StatusCode -eq 0){
+                    Write-host "UDP $port is Open on $IP Machine"
+                $OpenPort = [PSCustomObject]@{
+                    Proto = "UDP"
+                    Port = $port
+                    Host = $IP
+                    Status = "Filtered"
+                }
+                $OpenPortList += $OpenPort
+                } # Close PingStatus IF block
+            } elseif ($Error[0].ToString() -match "forcibly closed") {
+                Write-host "UDP $port is Closed on $IP Machine"
+                $OpenPort = [PSCustomObject]@{
+                    Proto = "UDP"
+                    Port = $port
+                    Host = $IP
+                    Status = "Closed"
+                }
+                $OpenPortList += $OpenPort
+            }# Close Error If block
+        } #Close Catch Block
+    $udpObject.Close()
+    } # Close Ports Foreach Block
+
 } #close IP foreach loop
 
-$OpenPortList
+$OpenPortList | eport-csv .\OpenPortList.csv
 
-#$wait = $tcpConnection.AsyncWaitHandle.WaitOne(1000, $false)
-
-$IP = "10.4.1.180"
-$Port = 443
-
-$OpenPortList = @()
-
-$tcp = New-Object System.Net.Sockets.TcpClient
-$tcpconnection = $tcp.ConnectAsync($IP,$port) 
-$wait = $tcpConnection.AsyncWaitHandle.WaitOne(1000, $false)
- 
-if (!$wait){
-    Write-host "$port is closed on $IP Machine"
-}
-else {
-    Write-host "$port is Open on $IP Machine"
-    $OpenPort = [PSCustomObject]@{
-        Port = $port
-        Host = $IP
-    }
-
-    $OpenPortList += $OpenPort
-    $tcpConnection.Dispose()  
-} 
-
-$tcp.Dispose()
-
-##############################################################
-# UDP Configuration Here
-##############################################################
-
-# [int]$UDPports = 53, 88
